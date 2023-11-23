4a722cd47cfe715e6c34e94a17a4358ce7d75924
FearlessAtom <atomgotaggressive@gmail.com>
2023-11-23 18:47:55 +0200

    23.11,18:47

---
+++ task2/task2/task2.cpp
@@ -0,0 +1,37 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <limits.h>
+int main() {
+	srand(time(0));
+	int number = 5;
+	int** array = (int**)malloc(sizeof(int*) * number);
+	for (int index = 0; index < number; index++) {
+		array[index] = (int*)malloc(sizeof(int) * number);}
+	int* pointer = &array[0][0];
+	for (int index = 0; index < number; index++) {
+		for (int j = 0; j < number; j++) {
+			*(pointer + index * number + j) = rand() % 20 - 10;
+			printf("%4d", *(pointer + index * number + j));}
+		printf("\n");}
+	int min = *pointer;
+	int minindex, minj;
+	for (int index = 0; index < number; index++) {
+		for (int j = 0; j < number; j++) {
+			if (*(pointer + index * number + j) < min) {
+				min = *(pointer + index * number + j);
+				minindex = index;
+				minj = j;}}}
+	printf("Min = %d\n", min);
+	for (int index = minindex; index < number - 1; index++) {
+		for (int j = 0; j < number; j++) {
+			*(pointer + index * number + j) = *(pointer + (index + 1) * number + j);}}
+	for (int j = minj; j < number - 1; j++) {
+		for (int index = 0; index < number; index++) {
+			*(pointer + index * number + j) = *(pointer + index * number + (j + 1));}}
+	for (int index = 0; index < number - 1; index++) {
+		for (int j = 0; j < number - 1; j++) {
+			printf("%4d", *(pointer + index * number + j));}
+		printf("\n");}
+	for (int index = 0; index < number; index++) { free(pointer + index); }
+	return 0;}
---
+++ task3.cpp
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+int main() {
+    srand(time(0));
+    int count = 0;
+    int number = 5;
+    int *arrayone = (int *)malloc(sizeof(int) * number);
+    int *pointerone = arrayone;
+    printf("First array : \n");
+    for (int index = 0; index < number; index++) {
+        *(pointerone + index) = rand() % 5;
+        printf("%2d", *(pointerone + index));}
+    int *arraytwo = (int *)malloc(sizeof(int) * number);
+    int *pointertwo = arraytwo;
+    printf("\nSecond array : \n");
+    for (int index = 0; index < number; index++) {
+        *(pointertwo + index) = rand() % 5;
+        printf("%2d", *(pointertwo + index));}
+    printf("\n");
+    int *both = (int *)malloc(sizeof(int) * number * 2);
+    int *pointerboth = both;
+    for (int index = 0; index < number; index++) {*(pointerboth + index) = *(pointerone + index);}
+    for (int index = 0; index < number; index++) {*(pointerboth + index + number) = *(pointertwo + index);}
+    printf("Merged array : \n");
+    for (int index = 0; index < number * 2; index++) {printf("%2d", *(pointerboth + index));}
+    int *common = (int *)malloc(sizeof(int) * number);
+    int *pointercommon = common;
+    printf("\nCommon elements : \n");
+    for (int i = 0; i < number; i++) {
+        for (int j = 0; j < number; j++) {
+            if (*(pointerone + i) == *(pointertwo + j)) {
+                int found = 0;
+                for (int k = 0; k < count; k++) {
+                    if (*(pointerone + i) == *(pointercommon + k)) {
+                        found = 1;
+                        break;}}
+                if (!found) {
+                    *(pointercommon + count) = *(pointerone + i);
+                    count++;}}}}
+    for (int index = 0; index < count; index++) {printf("%2d", *(pointercommon + index));}
+    free(arrayone);
+    free(arraytwo);
+    free(both);
+    free(common);
+    return 0;}
